8a805a0ecf3abeb8a36df2ee799a2916dfdaf954
dominicklenk <dominic_klenk@yahoo.at>
2018-06-26 14:07:02 +0200

    exercise intermediate finished

--- ex-interm/script.js
+++ ex-interm/script.js
@@ -1,7 +1,7 @@
 function loadxml() {
 	var xhttp = new XMLHttpRequest;
 	xhttp.onreadystatechange = function() {
-		if(this.stateChange == 4 && this.status == 200) {
+		if(this.readyState == 4 && this.status == 200) {
 			parsexml(this);
 		}
 	};
@@ -11,9 +11,15 @@ function loadxml() {
 function parsexml(xml) {
 	var i;
 	var xmldoc = xml.responseXML;
+	console.log(xml);
 	var table = "";
 	var songs = xmldoc.getElementsByTagName("song");
 		for (i = 0; i < songs.length; i++) {
-			table += "<tr><td>" + song[i].getElementsByTagName("title")[0].childNodes[0].nodeValue + "</td><td>"
+			table += "<tr><td>" + songs[i].getElementsByTagName("title")[0].childNodes[0].nodeValue + "</td><td>"
+			+ songs[i].getElementsByTagName("artist")[0].childNodes[0].nodeValue + "</td><td>"
+			+ songs[i].getElementsByTagName("country")[0].childNodes[0].nodeValue + "</td><td>"
+			+ songs[i].getElementsByTagName("genre")[0].childNodes[0].nodeValue + "</td><td>"
+			+ songs[i].getElementsByTagName("year")[0].childNodes[0].nodeValue + "</td><td>";
 		}
+	document.getElementById("content").innerHTML = table;
 	}
